d7018be5692566c503029cbcc70ff5cf74be86a2
unknown <you@example.com>
2019-10-05 17:04:24 +0200

    Added README and gitignore

---
+++ README.md
@@ -0,0 +1,3 @@
+# To-Do App
+
+Hi,this is a to-do app.
--- js/index.js
+++ js/index.js
@@ -5,35 +5,40 @@ let taskInput = document.getElementById("task-input");
 let addTaskBtn = document.getElementById("add-task-btn");
 let taskList = document.getElementById("task-list");
 
-taskInput.addEventListener("keypress",submitButton)
+taskInput.addEventListener("keypress",submitButton);
 addTaskBtn.addEventListener("click",addTask);
+taskList.addEventListener("click",finishedTasks);
 
 function submitButton(e){
     e.keyCode === 13 ? addTask() : null;
 }
 
 function addTask(){
-    if(taskInput.value === ""){
-        alert("Enter task first")
-    }
-    else{
-        task = `
-        <li class="list-group-item">
-          ${taskInput.value}
-          <input type="checkbox">
-          <button type="button" class="close" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </li>`
-        task.id = counter;
-        taskList.innerHTML += task;
-        task = ``;
-        counter++;
-    }
+  if(taskInput.value === ""){
+    alert("Enter task first")
+  }
+  else{
+    task = document.createElement("li");
+    task.innerHTML = `
+    ${taskInput.value}
+    <input type="checkbox">
+    <button type="button" class="close" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>`
+    task.id = `task-${counter}`;
+    task.className += "list-group-item";
+    taskList.appendChild(task);
+    task = ``;
+    counter++;
+  }
 }
 
 function finishedTasks(e){
   if(e.target.type === "checkbox"){
-    
+    e.target.parentElement.style.textDecoration = e.target.checked
+    ? "line-through" : "none";
+  }
+  else if(e.target.textContent === "&times;"){
+    taskList.removeChild(e.target.parentElement);
   }
 }
