1d52534ce384354ed2ad22a32bab2354a274b011
unknown <you@example.com>
2019-10-05 21:32:20 +0200

    Changed index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 
-let counter = 0;
-let task = ``;
 let taskInput = document.getElementById("task-input");
 let addTaskBtn = document.getElementById("add-task-btn");
 let taskList = document.getElementById("task-list");
+let counter = 0;
+let task = ``;
+let taskCounter = 0;
+let taskName = ``;
+let checkboxValue = ``;
+let jsonData = [];
 
 taskInput.addEventListener("keypress",submitButton);
 addTaskBtn.addEventListener("click",addTask);
 taskList.addEventListener("click",finishedTasks);
+document.getElementById("save-changes").addEventListener("click",saveTasks);
 
 function submitButton(e){
     e.keyCode === 13 ? addTask() : null;
@@ -21,14 +26,11 @@ function addTask(){
     task = document.createElement("li");
     task.innerHTML = `
     ${taskInput.value}
-    <input type="checkbox">
-    <button type="button" class="close" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
-    </button>`
+    <input id=checkbox-${counter} type="checkbox">
+    <button type="button" class="close" aria-label="Close">&times;</button>`;
     task.id = `task-${counter}`;
     task.className += "list-group-item";
     taskList.appendChild(task);
-    task = ``;
     counter++;
   }
 }
@@ -38,7 +40,25 @@ function finishedTasks(e){
     e.target.parentElement.style.textDecoration = e.target.checked
     ? "line-through" : "none";
   }
-  else if(e.target.textContent === "&times;"){
+  else if(e.target.tagName === "BUTTON"){
     taskList.removeChild(e.target.parentElement);
   }
+}
+
+function saveTasks(){
+  counter = 0;
+  taskCounter = taskList.children.length;
+  for(counter;counter<taskCounter;counter++){
+    taskName = document.getElementById(`task-${counter}`).innerText;
+    checkboxValue = document.getElementById(`checkbox-${counter}`).value;
+    jsonData += `
+    {
+      "title" = "${taskName}",
+      "done" = ${checkboxValue},
+    }`;
+  }
+  console.log(jsonData);
+  jsonData = [];
+  JSON.stringify(jsonData);;
+  
 }
