fb8665dd9554a8851fdda457f3faefa58d44aad6
unknown <you@example.com>
2019-10-14 19:25:53 +0200

    Finished index.js

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ function addTask(){
     task = document.createElement("li");
     task.innerHTML = `
     <span id="task-title-${counter}">${taskInput.value}</span>
-    <input id=checkbox-${counter} type="checkbox">
+    <input id="checkbox-${counter}" type="checkbox">
     <button type="button" class="close" aria-label="Close">&times;</button>`;
     task.id = `task-${counter}`;
     task.className += "list-group-item";
@@ -49,11 +49,12 @@ function saveTasks(e){
   let jsonData = [];
   counter = 0;
   let taskCounter = taskList.children.length;
-  for(counter;counter<taskCounter;counter++){
-    taskName = document.getElementById(`task-title-${counter}`).innerText;
-    checkboxValue = document.getElementById(`checkbox-${counter}`).checked;
-    jsonData.push({title:taskName,done:checkboxValue});
-    console.log(jsonData);
+  for(counter;counter<=taskCounter;counter++){
+    if(document.getElementById(`task-${counter}`) !== null){
+      taskName = document.getElementById(`task-title-${counter}`).innerText;
+      checkboxValue = document.getElementById(`checkbox-${counter}`).checked;
+      jsonData.push({title:taskName,done:checkboxValue});
+    }
   }
   fetch("https://api.myjson.com/bins",{
     method:"POST",
@@ -63,23 +64,23 @@ function saveTasks(e){
     },
     body:JSON.stringify(jsonData)
   })
-  .then(res => res.json(),)
-  .then((data) => console.log(data))
-  .then(localStorage.setItem("userTasks",`${JSON.parse(res.json.uri)}`))
-}/*
-//load saved tasks,not finished
-if(localStorage.length > 0){
+  .then(res => res.json())
+  .then((data) => {
+    localStorage.setItem("userTasks", data.uri);
+  })
+}
+//load saved tasks
+if(localStorage.getItem("userTasks") !== null){
   fetch(`${localStorage.getItem("userTasks")}`)
   .then(res => res.json())
   .then(data => {
-    console.log(data);
     for(counter=0;counter<data.length;counter++){
       taskList.innerHTML += `
-      <li class="list-group-item">
-        <span>${data[counter].title}</span>
-        <input type="checkbox">
+      <li id="task-${counter}" class="list-group-item">
+        <span id="task-title-${counter}">${data[counter].title}</span>
+        <input id="checkbox-${counter}" type="checkbox" ${data[counter].done === true ? "checked" : null}>
         <button type="button" class="close" aria-label="Close">&times;</button>
       </li>`;
     }
   })
-}*/
+}
